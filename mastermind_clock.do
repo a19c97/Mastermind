141698c6890dac8b59d1112cee8f58ffa4d0a5aa
AsicChen <asic.chen@mail.utoronto.ca>
2017-03-27 14:07:00 -0400

    slow clock module working

--- mastermind_clock.do
+++ mastermind_clock.do
@@ -1,9 +1,9 @@
 vlib work
-vlog -timescale 1ns/1ns mastermind_rate_divider.v
-vsim rate_divider
+vlog -timescale 1ns/1ns mastermind_slow_clock.v
+vsim slow_clock
 log {/*}
 add wave {/*}
 
 force {clock} 0 0, 1 1 -repeat 2
-force (reset_n} 1 0, 0 2, 1 4
+force {reset_n} 1 0, 0 2, 1 4
 run 100ns
--- mastermind_slow_clock.v
+++ mastermind_slow_clock.v
@@ -1,8 +1,8 @@
-module rate_divider(reset_n, clock, slow_clock); 
+module slow_clock(reset_n, clock, slow_clock); 
 
 	input clock;
 	input reset_n;
-	output reg slowclock;
+	output reg slow_clock;
 	reg [27:0] q;
 	
 	always @(posedge clock)
@@ -14,14 +14,17 @@ module rate_divider(reset_n, clock, slow_clock);
 		end
 		else
 		begin
-			//if (q == (20'b11110100001001000000 - 1))
-			if (q == 3'b4)
+			if (q == (20'b11110100001001000000 - 1))
+			//if (q == 3'd4)
 				begin
 				q <= 0;
 				slow_clock <= 1'b1;
 				end
 			else
+				begin
 				q <= q + 1'b1;
+				slow_clock <= 1'b0;
+				end
 		end
 	end
 endmodule
