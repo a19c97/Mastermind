18770ec69fc8e108ab3a22cd1d3a8bda0eaf1da3
jasonc-piano <jasonc.piano@gmail.com>
2017-03-29 19:54:42 -0400

    Fix numbers

--- vga_adapter/mastermind.v
+++ vga_adapter/mastermind.v
@@ -405,6 +405,8 @@ module mastermind_datapath(
     wire [8:0] big_x, big_y;
     wire [6:0] medium_x, medium_y;
     wire [11:0] erase_x, erase_y;
+    wire [6:0] small_red_x, small_red_y;
+    wire [6:0] small_white_x, small_white_y;
     
     big_square bs_counter(
     	.enable(load_code_1 || load_code_2 || load_code_3 || load_code_4),
@@ -422,12 +424,30 @@ module mastermind_datapath(
     	.y(medium_y)
     );
     
-    erase_code_squares(
+    erase_code_squares erase_counter(
     	.enable(erase_code),
     	.clock(fast_clk),
     	.resetn(resetn),
     	.x(erase_x),
     	.y(erase_y)
+    );
+
+    small_squares red_counter(
+        .enable(draw_result_1),
+        .clock(fast_clk),
+        .resetn(resetn),
+        .peg_count(red_out),
+        .x(small_red_x),
+        .y(small_red_y)
+    );
+
+    small_squares white_counter(
+        .enable(draw_result_2),
+        .clock(fast_clk),
+        .resetn(resetn),
+        .peg_count(white_out),
+        .x(small_white_x),
+        .y(small_white_y)
     );
 
     // Drawing always block 
@@ -484,8 +504,10 @@ module mastermind_datapath(
             end
 
             if (erase_code) begin
-                x_out <= 7'd10 + erase_x[6:0];
-                y_out <= 7'd50 + erase_y[6:0];
+                //x_out <= 7'd10 + erase_x[6:0];
+                //y_out <= 7'd50 + erase_y[6:0];
+                x_out <= 7'd0 + erase_x[6:0];
+                y_out <= 7'd0 + erase_y[6:0];
                 colour_out <= 3'b000;
             end
 
@@ -495,8 +517,8 @@ module mastermind_datapath(
             	else
 					colour_out <= 3'b100; // Draw red
 					
-		        x_out <= 7'd100;
-	            y_out <= 7'd10 + (7'd15 * {4'b0, guess_counter});
+		        x_out <= 7'd100 + small_red_x;
+	            y_out <= 7'd10 + (7'd15 * {4'b0, guess_counter}) + small_red_y;
             end
 
             if (draw_result_2) begin
@@ -505,8 +527,8 @@ module mastermind_datapath(
             	else
             		colour_out <= 3'b111; // Draw white
            		
-                x_out <= 7'd100;
-                y_out <= 7'd16 + (7'd15 * {4'b0, guess_counter});
+                x_out <= 7'd100 + small_white_x;
+                y_out <= 7'd16 + (7'd15 * {4'b0, guess_counter}) + small_white_y;
                 
             end
         end
@@ -775,8 +797,8 @@ module big_square(
 );
     reg [8:0] Q;
     
-    assign x = Q % 9'b000010100; // modulo 20
-    assign y = Q / 9'b000010100; // floor division by 20
+    assign x = Q % 9'd20; // modulo 20
+    assign y = Q / 9'd20; // floor division by 20
 
     always @(posedge clock)
     begin
@@ -784,7 +806,7 @@ module big_square(
             Q <= 0;
         else if (enable == 1'b1)
         begin
-            if (Q == 9'b110001111) // 399 in binary
+            if (Q == 9'd399) // 399 in binary
                 Q <= 0;
             else
                 Q <= Q + 1'b1;
@@ -803,8 +825,8 @@ module medium_square(
 );
     reg [6:0] Q;
     
-    assign x = Q % 7'b0001010; // modulo 10
-    assign y = Q / 7'b0001010; // floor division by 10
+    assign x = Q % 7'd10; // modulo 10
+    assign y = Q / 7'd10; // floor division by 10
 
     always @(posedge clock)
     begin
@@ -812,7 +834,7 @@ module medium_square(
             Q <= 0;
         else if (enable == 1'b1)
         begin
-            if (Q == 7'b1100011) // 99 in binary
+            if (Q == 7'd99) // 99 in binary
                 Q <= 0;
             else
                 Q <= Q + 1'b1;
@@ -829,10 +851,11 @@ module erase_code_squares(
     output [11:0] x,
     output [11:0] y
 );
-    reg [6:0] Q;
+
+    reg [11:0] Q;
     
-    assign x = Q % 12'b000001101110; // modulo 110
-    assign y = Q / 12'b000001101110; // floor division by 110
+    assign x = Q % 12'd110; // modulo 110
+    assign y = Q / 12'd110; // floor division by 110
 
     always @(posedge clock)
     begin
@@ -840,7 +863,106 @@ module erase_code_squares(
             Q <= 0;
         else if (enable == 1'b1)
         begin
-            if (Q == 12'b100010010111) // 2199 in binary
+            if (Q == 12'd2199) // 2199 in binary
+                Q <= 0;
+            else
+                Q <= Q + 1'b1;
+        end
+    end 
+	/*
+	// the erase everything version
+	reg [7:0] Q;
+	assign x = Q % 4'd10;
+	assign y = Q / 4'd10;
+	always @(posedge clock)
+    begin
+        if (!resetn)
+            Q <= 0;
+        else if (enable == 1'b1)
+        begin
+            if (Q == 8'd160)
+                Q <= 0;
+            else
+                Q <= Q + 1'b1;
+        end
+    end
+
+endmodule */
+
+
+module small_squares(
+    input enable,
+    input clock, // 50 MHz clock
+    input resetn,
+    input [2:0] peg_count,
+    output reg [6:0] x,
+    output [6:0] y
+);
+    reg [6:0] Q;
+    
+    always @(*) begin
+        if (!resetn)
+            x <= 0;
+        else begin
+            case (peg_count)
+                3'd0: begin
+                    x <= 7'b0;
+                end
+                3'd1: begin
+                    if (Q % 7'd22 >= 7'd4)
+                        x <= 0;
+                    else
+                        x <= Q % 7'd22;
+                end
+                
+                3'd2: begin
+                    if (Q % 7'd22 >= 7'd10)
+                        x <= 0;
+                    else if (Q % 7'd22 == 7'd4 ||
+                             Q % 7'd22 == 7'd5)
+                        x <= 0;
+                    else
+                        x <= Q % 7'd22;
+                end
+                
+                3'd3: begin
+                    if (Q % 7'd22 >= 7'd16)
+                        x <= 0;
+                    else if (Q % 7'd22 == 7'd4 ||
+                             Q % 7'd22 == 7'd5 ||
+                             Q % 7'd22 == 7'd10 ||
+                             Q % 7'd22 == 7'd11)
+                        x <= 0;
+                    else
+                        x <= Q % 7'd22;
+                end
+                
+                3'd4: begin
+                    if (Q % 7'd22 == 7'd4 ||
+                             Q % 7'd22 == 7'd5 ||
+                             Q % 7'd22 == 7'd10 ||
+                             Q % 7'd22 == 7'd11 ||
+                             Q % 7'd22 == 7'd16 ||
+                             Q % 7'd22 == 7'd17)
+                        x <= 0;
+                    else
+                        x <= Q % 7'd22;
+                end
+
+                default: x <= 0;
+            endcase
+        end
+    end
+
+    assign y = Q / 7'd22; // floor division by 22
+
+    always @(posedge clock)
+    begin
+        if (!resetn)
+            Q <= 0;
+        else if (enable == 1'b1)
+        begin
+            if (Q == 7'd87) // 87 in binary
                 Q <= 0;
             else
                 Q <= Q + 1'b1;
@@ -848,5 +970,4 @@ module erase_code_squares(
     end
 
 endmodule
-
 
